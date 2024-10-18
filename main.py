9548c72cb8296c866803f0a6ee8b64729b8fe341
Amina <aminayegnb@gmail.com>
2024-10-18 22:11:48 +0500

    Added getpost by id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,17 +12,26 @@ class Post(BaseModel):
     publish: bool = True
     rating: Optional[int] = None
 
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
 @app.get("/")
 def root():
     return {"message": "Hello//"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(new_post: Post):
-    print(new_post)
-    print(new_post.model_dump())
-    return {"data": new_post}
-# title str, content str 
+    post_dict = new_post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    print(id)
+    return {"post_detail": f"post id - {id}"}
