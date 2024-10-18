3e71cef14fd4cd719f88bdb43e80e8abcfb27872
Amina <aminayegnb@gmail.com>
2024-10-18 22:57:36 +0500

    Added update

--- main.py
+++ main.py
@@ -60,3 +60,14 @@ def delete_post(id: int):
                             detail=f"post with id - {id} is not found")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, upost: Post):
+    index = find_index_post(id)
+    if index == -1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id - {id} is not found")
+    post_dict = upost.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
